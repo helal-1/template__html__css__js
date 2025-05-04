9cf2d8d4015326a93046799c2d4118ce35924ea1
helal-1 <mh0036790@gmail.com>
2025-05-04 17:22:03 +0300

    update slider hero banner

--- Assets/Scripet__Js/main.js
+++ Assets/Scripet__Js/main.js
@@ -61,13 +61,13 @@ function currentSlide(n) {
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("image__right");
+  let slides = document.querySelectorAll("#slider_hero");
   if (n > slides.length) { slideIndex = 1 }
   if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1].style.display = "flex";
 
 }
 
