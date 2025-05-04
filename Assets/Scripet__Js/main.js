94b77586305f5c536ebac3975ff6d79a814671f7
helal-1 <mh0036790@gmail.com>
2025-05-04 18:57:16 +0300

    menu login

--- Assets/Scripet__Js/main.js
+++ Assets/Scripet__Js/main.js
@@ -88,3 +88,33 @@ nextBtn.addEventListener("click" ,()=>{
   scrollContainer.scrollLeft += 500
 
 });
+
+// password
+const pass = document.getElementById('password');
+const btn_eye = document.getElementById('eye_open');
+const icon = document.getElementById("icon");
+btn_eye.addEventListener('click', () => {
+  if (pass.type === 'password') {
+    pass.type = 'text';
+    icon.classList.remove('bi-eye');
+    icon.classList.add('bi-eye-slash');
+  } else {
+    pass.type = 'password';
+    icon.classList.add('bi-eye');
+    icon.classList.remove('bi-eye-slash');
+  }
+});
+
+// menu log in
+let login = document.querySelector(".login");
+let account = document.querySelector(".account");
+let close_menu_login = document.getElementById("close_menu_login");
+function open_log() {
+  console.log("hello");
+  account.style.display = "flex"
+}
+function close_log() {
+  console.log("hello");
+  account.style.display = "none"
+
+}
